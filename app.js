8375bd81a226c76658123a719b6444669215c2a0
RyznKey <ahasanmaulanar@gmail.com>
2022-08-11 06:44:45 +0700

    lanjutan navbar

---
+++ app.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+const mobileMenu = document.querySelector(".header .nav-bar .nav-list ul");
+const menuItem = document.querySelectorAll(".header .nav-bar .nav-list ul li a")
+const header = document.querySelector(".header.container");
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("active")
+    mobileMenu.classList.toggle("active")
+})
+
+// document.addEventListener('scroll', () => {
+//     var scroll_psition = window.scrollY;
+//     if (scroll_psition)
+// })
+
+document.addEventListener('scroll', () =>{
+    var scrollPosition =  window.scrollY;
+    if (scrollPosition > 250){
+        header.style.backgroundColor = "#29323c"
+    }else{
+        header.style.backgroundColor = "transparent"
+    }
+})
+
+menuItem.forEach((item) =>{
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle("active")
+        mobileMenu.classList.toggle("active")
+    })
+})
